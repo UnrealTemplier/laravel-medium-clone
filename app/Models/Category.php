c0b7edd22c09b5ade714cda03f7894f86c4f2489
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-22 17:30:48 +0400

    Add PostFactory.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use ;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
+/** @mixin Builder */
 class Category extends Model
 {
     //
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
+/** @mixin Builder */
 class Post extends Model
 {
-    //
+    use HasFactory;
 }
