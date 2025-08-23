468420d972e0fe1bcbcdd066a64417425a990f70
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-23 16:56:13 +0400

    Extract /posts/create request validation to PostCreateRequest class.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostCreateRequest;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -32,15 +33,9 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        $data = $request->validate([
-            'image'        => 'required|image|mimes:jpg,jpeg,png,svg|max:2048',
-            'title'        => 'required',
-            'content'      => 'required',
-            'category_id'  => 'required|exists:categories,id',
-            'published_at' => 'nullable|date'
-        ]);
+        $data = $request->validated();
 
         /** @var UploadedFile $image */
         $image         = $data['image'];
