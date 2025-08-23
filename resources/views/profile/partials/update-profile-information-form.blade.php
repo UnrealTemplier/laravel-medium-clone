1b7ab87f40c51cacca1bba96ac5427434ff35bf9
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-23 21:57:39 +0400

    Fix missing brace.

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -19,7 +19,7 @@
 
         @if($user->avatar)
             <div>
-                <img src="{{ $user->avatarUrl()) }}" alt="{{ $user->name }}" class="rounded w-20 h-20"/>
+                <img src="{{ $user->avatarUrl() }}" alt="{{ $user->name }}" class="rounded w-20 h-20"/>
             </div>
         @endif
 
